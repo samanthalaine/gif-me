c5bdb6104faf9d0a837375d9ee750f2b6c3628aa
Samantha Laine <samanthalaine8@gmail.com>
2021-11-12 00:59:58 -0500

    fetched data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [gifs, setGifs] = useState([])
+
+  useEffect(() => {
+    const fetchGifs = async () =>{
+      const res = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIPHY_KEY}&limit=25&rating=g`)
+      const data = await res.json()
+      setGifs(data)
+    }
+    fetchGifs()
+  }, [])
+
+
   return (
     <div className="text-3xl">
     <h1>hello world</h1>
