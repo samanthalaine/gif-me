2011a5ef74ec3d3861c808a0552588476eed1edf
Samantha Laine <samanthalaine8@gmail.com>
2021-11-12 23:46:26 -0500

    formatted date on gif card

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
-function GifCard({gif}) {
-    return (
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-        <Link to={`/gif/${gif.id}`}><img class="w-full" src={gif.images.fixed_width.url} alt="" className="w-full"/></Link>
-        <div className="px-6 py-3 bg-white">
-        <div className="font-bold text-blue-700 text-xl mb-2">
-          {gif.title}
-        </div>
+
+function GifCard({ gif }) {
+  let date = new Date(gif.import_datetime);
+  let formatDate = date.toLocaleString("en-us");
+  //date object example: (10/21/2021, 3:58:47 PM)
+  //splits date object into array, then returns first element of that array
+  formatDate = formatDate.split(",")[0];
+  console.log(formatDate);
+
+  return (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+      <Link to={`/gif/${gif.id}`}>
+        <img
+          class="w-full"
+          src={gif.images.fixed_width.url}
+          alt=""
+          className="w-full"
+        />
+      </Link>
+      <div className="px-6 py-3 bg-white">
+        <div className="font-bold text-blue-700 text-xl mb-2">{gif.title}</div>
         <ul>
           <li>
             <strong>Username: </strong>
@@ -18,16 +31,15 @@ function GifCard({gif}) {
             <strong>Rating: </strong>
             {gif.rating}
           </li>
-         
         </ul>
-        </div>
-        <div className="px-6 py-3 bg-white">
-          <span className="inline-block bg-green-300 rounded full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
-            {gif.import_datetime}
-          </span>
-        </div>
       </div>
-    )
+      <div className="px-6 py-3 bg-white">
+        <span className="inline-block bg-green-300 rounded full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
+          Posted on: {formatDate}
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default GifCard
+export default GifCard;
