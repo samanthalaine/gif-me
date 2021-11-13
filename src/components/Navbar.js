8e64d8ba9e40dfbd536273d6431e3f8f27c4d5af
Samantha Laine <samanthalaine8@gmail.com>
2021-11-13 14:09:42 -0500

    fixed mobile navbar menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,39 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [isOpen, setisOpen] = useState(false);
+
+  const showMenu = () => {
+    isOpen ? setisOpen(false) : setisOpen(true);
+  };
+
   return (
     <div>
       <nav class="flex items-center bg-gray-900 p-3 flex-wrap">
         <Link to="/" class="p-2 mr-4 inline-flex items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-</svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-gray-200"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
           <span class="text-xl px-2 text-white font-bold uppercase tracking-wide">
             GifMe
           </span>
         </Link>
         <button
+          onClick={() => {
+            showMenu();
+          }}
           class="text-white inline-flex p-3 hover:bg-gray-900 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
           data-target="#navigation"
         >
@@ -32,7 +52,27 @@ function Navbar() {
             />
           </svg>
         </button>
-        <div
+
+       { isOpen ? <div
+          class="top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto"
+          id="navigation"
+        >
+          <div class="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
+            <Link
+              to="/search"
+              class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+            >
+              <span>Search</span>
+            </Link>
+            <Link
+              to="/upload"
+              class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+            >
+              <span>Upload</span>
+            </Link>
+          </div>
+        
+        </div> : <div
           class="hidden top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto"
           id="navigation"
         >
@@ -50,7 +90,8 @@ function Navbar() {
               <span>Upload</span>
             </Link>
           </div>
-        </div>
+        
+        </div>}
       </nav>
     </div>
   );
