bf5ff29d5a07024eb8916cf7f0c5786afa405fad
Samantha Laine <samanthalaine8@gmail.com>
2021-11-13 12:37:56 -0500

    edited upload styling and images

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -26,22 +26,32 @@ function Upload() {
   };
 
   return (
-    <div>
-      <h1 className="text-xl text-blue-600">Upload Gif</h1>
+    <div className="">
+      <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-gray-900 rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-blue-500">
+      <h1 className="text-xl font-semibold mb-5 text-gray-600">Upload a Gif</h1>
       <input
         type="file"
         name="file"
         placeholder="Upload a gif"
         onChange={uploadGif}
+        class="hidden"
       />
-
-      {loading ? (
-        <h1 className="text-4xl text-center text-gray-500 mx-auto mt-32">Loading...</h1>
-      ) : (
-          
-        gif.map((gif) => (
-        <img src={gif.secure_url} />)
-        ))}
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+</svg>
+      </label>
+      {loading ? 
+        <h1 className="text-4xl text-center text-gray-500 mx-auto mt-32">
+          Loading...
+        </h1>
+       : 
+       <div className="grid grid-cols-3 gap-4">{gif.map((gif) => (
+          <img
+            className="max-w-sm m-8 rounded overflow-hidden shadow-lg bg-white"
+            src={gif.secure_url}
+          />
+        ))
+      }</div>}
     </div>
   );
 }
